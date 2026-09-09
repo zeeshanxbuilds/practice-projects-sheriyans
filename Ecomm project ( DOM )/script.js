4735d4ca079c393d3ab25f196f684efb92a94ed1
Muhammad Zeeshan <mzeeshanbutt07@gmail.com>
2026-09-09 20:01:35 +0500

    Add product creation form and functionality with styling updates

--- Ecomm project ( DOM )/script.js
+++ Ecomm project ( DOM )/script.js
@@ -1,14 +1,21 @@
 const createBtn = document.querySelector('#create');
 const formDiv = document.querySelector('.form');
-
+const crossButton = document.querySelector('#cross_button')
+const form = document.querySelector('form')
 
 createBtn.addEventListener('click', () => {
     formDiv.style.display = 'flex';
 })
 
-// window.addEventListener('click', () => {
+crossButton.addEventListener('click', ()=> {
+    formDiv.style.display = 'none'
+})
 
-//     if(formDiv.style.display === 'flex'){
-//         formDiv.style.display = 'none';
-//     }
-// })
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log('hello')
+    
+    for(let i=0; i<=100; i++){
+        console.log('print hello')
+    }
+})
